b6ba81dc0d2ea59b3ee1239dec0caec0f481f61e
valva-ro <vavarela.rodriguez@gmail.com>
2021-06-26 17:07:54 -0300

    [ADD] possible resolution

--- js/index.js
+++ js/index.js
@@ -10,7 +10,11 @@ const jsonData = `
       "name": "Molecule Man",
       "age": 29,
       "secretIdentity": "Dan Jukes",
-      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
     },
     {
       "name": "Madame Uppercut",
@@ -40,7 +44,36 @@ const jsonData = `
 
 //Recorrer el JSON e insertar en el listado del html los nombres de los miembros
 function recorrerMiembros() {
-    //escribe la lógica
+  const miembros = JSON.parse(jsonData).members
+  miembros.forEach((miembro, i) => {
+    renderizarMiembro(miembro, i + 1);
+  });
 
 };
+
+function renderizarMiembro(miembro, id) {
+  const miembros = document.querySelector("ul");
+  const template = `
+      <li class="member" id=\"member-${id}\">
+        <h3>${miembro.name}</h3>
+        <div>Edad: ${miembro.age}</div>
+        <div>Identidad Secreta: ${miembro.secretIdentity}</div>
+        <div>Superpoderes:</div>
+      </li>
+  `;
+  miembros.innerHTML += template;
+  agregarSuperPoderes(miembro.powers, id);
+}
+
+function agregarSuperPoderes(superpoderes, id) {
+  const miembro = document.querySelector("#member-" + id);
+  let ul = document.createElement("ul");
+  superpoderes.forEach(superpoder => {
+    let li = document.createElement("li");
+    li.innerHTML = superpoder;
+    ul.appendChild(li);
+  })
+  miembro.appendChild(ul);
+}
+
 recorrerMiembros();
